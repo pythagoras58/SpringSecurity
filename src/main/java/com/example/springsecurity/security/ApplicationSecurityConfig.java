3532b85a2208570f44319022206a29b2bbb76a62
pythagoras58 <solomondanso58@gmail.com>
2022-10-01 21:23:11 +0000

    Ant Matchers

--- src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
@@ -13,6 +13,8 @@ public class ApplicationSecurityConfig  extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/", "index", "/css/", "/js/")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
---
+++ src/main/java/com/example/springsecurity/security/basicAuth.md
@@ -0,0 +1,3 @@
+*** NOTE:
+
+With basic auth, it is difficult to logout since every user request needs the username and password of the requester
