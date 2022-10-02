db7e7c030b2ae22b06704ce3603b391f098ababb
pythagoras58 <solomondanso58@gmail.com>
2022-10-02 17:33:43 +0000

    Password Encoder using BCrypt

--- src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.springsecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,11 +9,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig  extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -32,7 +41,7 @@ public class ApplicationSecurityConfig  extends WebSecurityConfigurerAdapter {
     protected UserDetailsService userDetailsService() {
         UserDetails pythagoras =  User.builder()
                 .username("pythagoras")
-                .password("admin")
+                .password(passwordEncoder.encode("admin"))
                 .roles("STUDENT") // ROLE_STUDENT
                 .build();
 
