8298bffe1f04fcf26401cb406a6d060b54fa92f8
pythagoras58 <solomondanso58@gmail.com>
2022-10-05 11:44:38 +0000

    Role based authentication

--- src/main/java/com/example/springsecurity/controllers/StudentController.java
+++ src/main/java/com/example/springsecurity/controllers/StudentController.java
@@ -3,12 +3,11 @@ package com.example.springsecurity.controllers;
 import com.example.springsecurity.model.StudentModel;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http:localhost:8099")
+//@CrossOrigin("http:localhost:8099")
 @RequestMapping("api/v1/students")
 public class StudentController {
 
@@ -22,7 +21,7 @@ public class StudentController {
 
 
 
-    @CrossOrigin("http:localhost:8099")
+//    @CrossOrigin("http:localhost:8099")
     @GetMapping(path = "{studentId}")
     public StudentModel getStudent(@PathVariable("studentId") Integer studentId){
         return STUDENT_MODEL_LIST.stream()
